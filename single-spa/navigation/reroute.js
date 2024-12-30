6587f2b66662a5f54280d48af27d3060aa6a152e
cc <2798750836@qq.com>
2024-12-30 21:24:49 +0800

    feat: 总结2.0

--- single-spa/navigation/reroute.js
+++ single-spa/navigation/reroute.js
@@ -10,15 +10,15 @@ export function reroute() {
     // 获取app状态，进行分类
     const { appsToLoad, appsToMount, appsToUnmount } = getAppChanges();
 
-
     // 先注册应用，应用注册完之后才会start。
     if (started) {
         // 用户调用了start方法，处理当前应用挂载卸载
         return performAppChange();
     }
 
-
     return loadApps();
+
+
 
     function loadApps() {
         // 根据需要加载的分类，加载对应的app
@@ -32,7 +32,6 @@ export function reroute() {
         // 加载需要的应用（这个应用注册时候可能没加载（因为只有路径对应的时候才会在注册的时候就加载）） -> 启动对应的应用 -> 挂载对应的应用
 
         const mountPromises = Promise.all(appsToLoad.map((app) => toLoadPromise(app).then((app) => {
-            // 当应用加载完毕后需要启动和挂载，但需要先保证卸载完毕
             return tryBootStrapAndMount(app, unmountAllPromises);
         })))
 
